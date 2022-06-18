970b64c52cee951d4db358c08017ce7254f26a24
L_B__ <73544345+ACking-you@users.noreply.github.com>
2022-06-18 23:51:07 +0800

    fix-bug:修复某些不需要鉴权的场景

--- router/router_douyin.go
+++ router/router_douyin.go
@@ -24,17 +24,17 @@ func InitDouyinRouter() *gin.Engine {
 	baseGroup.POST("/user/login/", middleware.SHAMiddleWare(), user_login.UserLoginHandler)
 	baseGroup.POST("/user/register/", middleware.SHAMiddleWare(), user_login.UserRegisterHandler)
 	baseGroup.POST("/publish/action/", middleware.JWTMiddleWare(), video.PublishVideoHandler)
-	baseGroup.GET("/publish/list/", middleware.JWTMiddleWare(), video.QueryVideoListHandler)
+	baseGroup.GET("/publish/list/", middleware.NoAuthToGetUserId(), video.QueryVideoListHandler)
 
 	//extend 1
 	baseGroup.POST("/favorite/action/", middleware.JWTMiddleWare(), video.PostFavorHandler)
-	baseGroup.GET("/favorite/list/", middleware.JWTMiddleWare(), video.QueryFavorVideoListHandler)
+	baseGroup.GET("/favorite/list/", middleware.NoAuthToGetUserId(), video.QueryFavorVideoListHandler)
 	baseGroup.POST("/comment/action/", middleware.JWTMiddleWare(), comment.PostCommentHandler)
 	baseGroup.GET("/comment/list/", middleware.JWTMiddleWare(), comment.QueryCommentListHandler)
 
 	//extend 2
 	baseGroup.POST("/relation/action/", middleware.JWTMiddleWare(), user_info.PostFollowActionHandler)
-	baseGroup.GET("/relation/follow/list/", middleware.JWTMiddleWare(), user_info.QueryFollowListHandler)
-	baseGroup.GET("/relation/follower/list/", middleware.JWTMiddleWare(), user_info.QueryFollowerHandler)
+	baseGroup.GET("/relation/follow/list/", middleware.NoAuthToGetUserId(), user_info.QueryFollowListHandler)
+	baseGroup.GET("/relation/follower/list/", middleware.NoAuthToGetUserId(), user_info.QueryFollowerHandler)
 	return r
 }
