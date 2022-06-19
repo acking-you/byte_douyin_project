7a00115db4ace4e79cb924c233acb3a2bba748a4
L_B__ <73544345+ACking-you@users.noreply.github.com>
2022-06-19 12:01:02 +0800

    Update user_info.go

--- models/user_info.go
+++ models/user_info.go
@@ -18,7 +18,7 @@ type UserInfo struct {
 	FollowCount   int64       `json:"follow_count" gorm:"follow_count,omitempty"`
 	FollowerCount int64       `json:"follower_count" gorm:"follower_count,omitempty"`
 	IsFollow      bool        `json:"is_follow" gorm:"is_follow,omitempty"`
-	User          *UserLogin  `json:"-"`                                     //用户与密码之间的多对多
+	User          *UserLogin  `json:"-"`                                     //用户与账号密码之间的一对一
 	Videos        []*Video    `json:"-"`                                     //用户与投稿视频的一对多
 	Follows       []*UserInfo `json:"-" gorm:"many2many:user_relations;"`    //用户之间的多对多
 	FavorVideos   []*Video    `json:"-" gorm:"many2many:user_favor_videos;"` //用户与点赞视频之间的多对多
